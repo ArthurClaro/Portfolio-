c65dbe37b598fe0e86b584c172e90b0cd350995c
Arthur <0123456789arthur@gmai.com>
2023-05-05 15:29:24 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,648 @@
+var loader = document.getElementById('preloader')
+
+window.addEventListener('load', function () {
+    setTimeout(() => {
+        loader.style.display = 'none'
+    }, "1300");
+})
+
+let h = document.querySelector("#primeiro")
+
+function entrada() {
+
+    h.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast").removeAttribute("id");
+}
+function saida() {
+
+
+    setTimeout(() => {
+        h.classList.remove(`animate__rubberBand`)
+    }, "500");
+
+}
+h.addEventListener("mouseover", entrada)
+h.addEventListener("mouseout", saida)
+
+// setTimeout(() => {
+//     document.querySelectorAll("span").removeAttribute("id");
+// }, "5000");
+
+
+
+let i = document.querySelector("#segundo")
+function entrada2() {
+
+    i.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast1").removeAttribute("id");
+}
+function saida2() {
+
+
+    setTimeout(() => {
+        i.classList.remove(`animate__rubberBand`)
+    }, "500");
+
+}
+i.addEventListener("mouseover", entrada2)
+i.addEventListener("mouseout", saida2)
+
+
+
+let terceiro = document.querySelector("#terceiro")
+function entrada3() {
+
+    terceiro.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast2").removeAttribute("id");
+}
+function saida3() {
+
+
+    setTimeout(() => {
+        terceiro.classList.remove(`animate__rubberBand`)
+    }, "500");
+
+}
+terceiro.addEventListener("mouseover", entrada3)
+terceiro.addEventListener("mouseout", saida3)
+
+
+let quarto = document.querySelector("#quarto")
+function entrada4() {
+
+    quarto.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast3").removeAttribute("id");
+}
+function saida4() {
+
+
+    setTimeout(() => {
+        quarto.classList.remove(`animate__rubberBand`)
+    }, "500");
+
+}
+quarto.addEventListener("mouseover", entrada4)
+quarto.addEventListener("mouseout", saida4)
+
+
+let quinto = document.querySelector("#quinto")
+function entrada5() {
+
+    quinto.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast4").removeAttribute("id");
+}
+function saida5() {
+
+
+    setTimeout(() => {
+        quinto.classList.remove(`animate__rubberBand`)
+    }, "500");
+
+}
+quinto.addEventListener("mouseover", entrada5)
+quinto.addEventListener("mouseout", saida5)
+
+
+
+let sexto = document.querySelector("#sexto")
+function entrada6() {
+
+    sexto.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast5").removeAttribute("id");
+}
+function saida6() {
+
+
+    setTimeout(() => {
+        sexto.classList.remove(`animate__rubberBand`)
+    }, "500");
+
+}
+sexto.addEventListener("mouseover", entrada6)
+sexto.addEventListener("mouseout", saida6)
+
+
+
+let sete = document.querySelector("#sete")
+function entrada7() {
+
+    sete.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast6").removeAttribute("id");
+}
+function saida7() {
+
+
+    setTimeout(() => {
+        sete.classList.remove(`animate__rubberBand`)
+    }, "500");
+
+}
+sete.addEventListener("mouseover", entrada7)
+sete.addEventListener("mouseout", saida7)
+
+
+let oito = document.querySelector("#oito")
+function entrada8() {
+    oito.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast7").removeAttribute("id");
+}
+function saida8() {
+    setTimeout(() => {
+        oito.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+oito.addEventListener("mouseover", entrada8)
+oito.addEventListener("mouseout", saida8)
+
+
+
+let nove = document.querySelector("#nove")
+function entrada9() {
+    nove.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast8").removeAttribute("id");
+}
+function saida9() {
+    setTimeout(() => {
+        nove.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+nove.addEventListener("mouseover", entrada9)
+nove.addEventListener("mouseout", saida9)
+
+
+let dez = document.querySelector("#dez")
+function entrada10() {
+    dez.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast9").removeAttribute("id");
+}
+function saida10() {
+    setTimeout(() => {
+        dez.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+dez.addEventListener("mouseover", entrada10)
+dez.addEventListener("mouseout", saida10)
+
+let onze = document.querySelector("#onze")
+function entrada11() {
+    onze.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast10").removeAttribute("id");
+}
+function saida11() {
+    setTimeout(() => {
+        onze.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+onze.addEventListener("mouseover", entrada11)
+onze.addEventListener("mouseout", saida11)
+
+//////////////////////////////////////////////////
+let doze = document.querySelector("#doze")
+function entrada12() {
+    doze.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast11").removeAttribute("id");
+}
+function saida12() {
+    setTimeout(() => {
+        doze.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+doze.addEventListener("mouseover", entrada12)
+doze.addEventListener("mouseout", saida12)
+//////////////////////////////////////////////////////
+
+let treze = document.querySelector("#treze")
+function entrada13() {
+    treze.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast12").removeAttribute("id");
+}
+function saida13() {
+    setTimeout(() => {
+        treze.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+treze.addEventListener("mouseover", entrada13)
+treze.addEventListener("mouseout", saida13)
+
+let quatorze = document.querySelector("#quatorze")
+function entrada14() {
+    quatorze.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast13").removeAttribute("id");
+}
+function saida14() {
+    setTimeout(() => {
+        quatorze.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+quatorze.addEventListener("mouseover", entrada14)
+quatorze.addEventListener("mouseout", saida14)
+
+let quize = document.querySelector("#quize")
+function entrada15() {
+    quize.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast14").removeAttribute("id");
+}
+function saida15() {
+    setTimeout(() => {
+        quize.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+quize.addEventListener("mouseover", entrada15)
+quize.addEventListener("mouseout", saida15)
+
+let dezesseis = document.querySelector("#dezesseis")
+function entrada16() {
+    dezesseis.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast15").removeAttribute("id");
+}
+function saida16() {
+    setTimeout(() => {
+        dezesseis.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+dezesseis.addEventListener("mouseover", entrada16)
+dezesseis.addEventListener("mouseout", saida16)
+
+let dezesete = document.querySelector("#dezesete")
+function entrada17() {
+    dezesete.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast16").removeAttribute("id");
+}
+function saida17() {
+    setTimeout(() => {
+        dezesete.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+dezesete.addEventListener("mouseover", entrada17)
+dezesete.addEventListener("mouseout", saida17)
+
+let dezoito = document.querySelector("#dezoito")
+function entrada18() {
+    dezoito.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast17").removeAttribute("id");
+}
+function saida18() {
+    setTimeout(() => {
+        dezoito.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+dezoito.addEventListener("mouseover", entrada18)
+dezoito.addEventListener("mouseout", saida18)
+
+let dezenove = document.querySelector("#dezenove")
+function entrada19() {
+    dezenove.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast18").removeAttribute("id");
+}
+function saida19() {
+    setTimeout(() => {
+        dezenove.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+dezenove.addEventListener("mouseover", entrada19)
+dezenove.addEventListener("mouseout", saida19)
+
+let vinte = document.querySelector("#vinte")
+function entrada20() {
+    vinte.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast19").removeAttribute("id");
+}
+function saida20() {
+    setTimeout(() => {
+        vinte.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+vinte.addEventListener("mouseover", entrada20)
+vinte.addEventListener("mouseout", saida20)
+
+let vinteum = document.querySelector("#vinteum")
+function entrada21() {
+    vinteum.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast20").removeAttribute("id");
+}
+function saida21() {
+    setTimeout(() => {
+        vinteum.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+vinteum.addEventListener("mouseover", entrada21)
+vinteum.addEventListener("mouseout", saida21)
+
+let vintedois = document.querySelector("#vintedois")
+function entrada22() {
+    vintedois.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast21").removeAttribute("id");
+}
+function saida22() {
+    setTimeout(() => {
+        vintedois.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+vintedois.addEventListener("mouseover", entrada22)
+vintedois.addEventListener("mouseout", saida22)
+
+let vintetres = document.querySelector("#vintetres")
+function entrada23() {
+    vintetres.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast22").removeAttribute("id");
+}
+function saida23() {
+    setTimeout(() => {
+        vintetres.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+vintetres.addEventListener("mouseover", entrada23)
+vintetres.addEventListener("mouseout", saida23)
+
+let vintequatro = document.querySelector("#vintequatro")
+function entrada24() {
+    vintequatro.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast23").removeAttribute("id");
+}
+function saida24() {
+    setTimeout(() => {
+        vintequatro.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+vintequatro.addEventListener("mouseover", entrada24)
+vintequatro.addEventListener("mouseout", saida24)
+
+let vintecinco = document.querySelector("#vintecinco")
+function entrada25() {
+    vintecinco.classList.add(`animate__rubberBand`)
+    document.querySelector(".blast24").removeAttribute("id");
+}
+function saida25() {
+    setTimeout(() => {
+        vintecinco.classList.remove(`animate__rubberBand`)
+    }, "500");
+}
+vintecinco.addEventListener("mouseover", entrada25)
+vintecinco.addEventListener("mouseout", saida25)
+
+////////////////////////////////////////////////////
+
+setTimeout(() => {
+
+    var typed = new Typed(".multiple", {
+        strings: ["Frontend Developer", "Blogger", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
+
+}, "4100");
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var PARTICLE_NUM = 500;
+var PARTICLE_BASE_RADIUS = 0.5;
+var FL = 500;
+var DEFAULT_SPEED = 2;
+var BOOST_SPEED = 300;
+
+var canvas;
+var canvasWidth, canvasHeight;
+var context;
+var centerX, centerY;
+var mouseX, mouseY;
+var speed = DEFAULT_SPEED;
+var targetSpeed = DEFAULT_SPEED;
+var particles = [];
+
+window.addEventListener('load', function () {
+    canvas = document.getElementById('c');
+
+    var resize = function () {
+        canvasWidth = canvas.width = window.innerWidth;
+        canvasHeight = canvas.height = window.innerHeight;
+        centerX = canvasWidth * 0.5;
+        centerY = canvasHeight * 0.5;
+        context = canvas.getContext('2d');
+        context.fillStyle = 'rgb(255, 255, 255)';
+    };
+
+    document.addEventListener('resize', resize);
+    resize();
+
+    mouseX = centerX;
+    mouseY = centerY;
+
+    for (var i = 0, p; i < PARTICLE_NUM; i++) {
+        particles[i] = randomizeParticle(new Particle());
+        particles[i].z -= 500 * Math.random();
+    }
+
+    document.addEventListener('mousemove', function (e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }, false);
+
+    document.addEventListener('mousedown', function (e) {
+        targetSpeed = BOOST_SPEED;
+    }, false);
+
+    document.addEventListener('mouseup', function (d) {
+        targetSpeed = DEFAULT_SPEED;
+    }, false);
+
+    setInterval(loop, 1000 / 60);
+}, false);
+
+function loop() {
+    context.save();
+    context.fillStyle = 'rgb(0, 0, 0)';
+    context.fillRect(0, 0, canvasWidth, canvasHeight);
+    context.restore();
+
+    speed += (targetSpeed - speed) * 0.01;
+
+    var p;
+    var cx, cy;
+    var rx, ry;
+    var f, x, y, r;
+    var pf, px, py, pr;
+    var a, a1, a2;
+
+    var halfPi = Math.PI * 0.5;
+    var atan2 = Math.atan2;
+    var cos = Math.cos;
+    var sin = Math.sin;
+
+    context.beginPath();
+    for (var i = 0; i < PARTICLE_NUM; i++) {
+        p = particles[i];
+
+        p.pastZ = p.z;
+        p.z -= speed;
+
+        if (p.z <= 0) {
+            randomizeParticle(p);
+            continue;
+        }
+
+        cx = centerX - (mouseX - centerX) * 1.25;
+        cy = centerY - (mouseY - centerY) * 1.25;
+
+        rx = p.x - cx;
+        ry = p.y - cy;
+
+        f = FL / p.z;
+        x = cx + rx * f;
+        y = cy + ry * f;
+        r = PARTICLE_BASE_RADIUS * f;
+
+        pf = FL / p.pastZ;
+        px = cx + rx * pf;
+        py = cy + ry * pf;
+        pr = PARTICLE_BASE_RADIUS * pf;
+
+        a = atan2(py - y, px - x);
+        a1 = a + halfPi;
+        a2 = a - halfPi;
+
+        context.moveTo(px + pr * cos(a1), py + pr * sin(a1));
+        context.arc(px, py, pr, a1, a2, true);
+        context.lineTo(x + r * cos(a2), y + r * sin(a2));
+        context.arc(x, y, r, a2, a1, true);
+        context.closePath();
+    }
+    context.fill();
+}
+
+function randomizeParticle(p) {
+    p.x = Math.random() * canvasWidth;
+    p.y = Math.random() * canvasHeight;
+    p.z = Math.random() * 1500 + 500;
+    return p;
+}
+
+function Particle(x, y, z) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.z = z || 0;
+    this.pastZ = 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function () {
+    $('#myCanvas').tagcanvas({
+        textColour: 'rgb(255, 255, 255)',
+        outlineThickness: 0.5,
+        outlineColour: '#fe0853',
+        maxSpeed: 0.06,
+        freezeActive: true,
+        shuffleTags: true,
+        shape: 'sphere',
+        zoom: 1.1,
+        noSelect: true,
+        textFont: null,
+        pinchZoom: true,
+        freezeDecel: true,
+        fadeIn: 3000,
+        initial: [0.3, -0.1],
+        depth: 0.8
+    });
+})
+
+//////////////////////////////////////////////////////////////////////
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var main = new Splide('#main-carousel', {
+
+        type: 'fade',
+        // padding: '5rem',
+        autoplay: true,
+        wheel: true,
+        arrows: false,
+        start: 1,
+
+
+
+    });
+
+    var thumbnails = new Splide('#thumbnail-carousel', {
+        fixedWidth: 200,
+        fixedHeight: 50,
+        gap: 10,
+        rewind: true,
+        pagination: false,
+        autoplay: true,
+
+        focus: 1.5,
+        isNavigation: true,
+        start: 1,
+        // arrows: false,
+
+        breakpoints: {
+            600: {
+                fixedWidth: 60,
+                fixedHeight: 44,
+            },
+        },
+    });
+
+    main.sync(thumbnails);
+    main.mount();
+    thumbnails.mount();
+});
+
+////////////////////////////////////////////////////////////////
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        direction: 'vertical',
+        loopBottom: true,
+        loopTop: true,
+        menu: '#navbar',
+        anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
+        // normalScrollElements: '.cima_skills',
+
+        navigation:
+            false,
+        // {
+        //     'textColor': '#fff',
+        //     'bulletsColor': '#fff',
+        //     'position': 'right',
+        //     'tooltips': ['Home', 'About', 'projects', 'Skils', 'Contact']
+        // },
+
+
+
+    });
+});
+
+/////////////////////////////////////////////////////////////
+
+var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+
+    // DESCOMENTAR AQUI !!!
+    autoplay: {
+        delay: 8000,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+///////////////////////////////////////////////////////
+
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1,
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// 
